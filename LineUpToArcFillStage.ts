8e487d656c5f57939c41275c51a00caa8168b9ae
Anwesh43 <anweshthecool0@gmail.com>
2018-10-06 12:35:11 +0530

    adding LineUpTOArcFillStage and State class

---
+++ LineUpToArcFillStage.ts
@@ -0,0 +1,54 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+
+class LineUpToArcFillStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#BDBDBD'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : LineUpToArcFillStage = new LineUpToArcFillStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
+
+class State {
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += 0.05 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
