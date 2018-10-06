fa19f5e9f15cf1f55940cd4ded8f768b44684e05
Anwesh43 <anweshthecool0@gmail.com>
2018-10-06 13:21:58 +0530

    adding LineUpToArcFill class

--- LineUpToArcFillStage.ts
+++ LineUpToArcFillStage.ts
@@ -141,3 +141,26 @@ class UTAFNode {
         return this
     }
 }
+
+class LineUpToArcFill {
+
+    root : UTAFNode = new UTAFNode(0)
+    curr : UTAFNode = this.root
+    dir : number = 1
+    draw(context : CanvasRenderingContext2D) {
+
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
